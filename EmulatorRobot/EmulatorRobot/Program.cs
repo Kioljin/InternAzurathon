1edc86f6e2d22033108893e85f293fcc99f25256
Matt Prince <runningfool5@gmail.com>
2014-07-23 15:45:08 -0700

    Added player 2 functionality -- NOT STABLE

--- EmulatorRobot/EmulatorRobot/Program.cs
+++ EmulatorRobot/EmulatorRobot/Program.cs
@@ -19,10 +19,44 @@ namespace EmulatorRobot
     {
         private int type;
         private int key;
-        public KeyboardEvent(NetworkStream stream)
+        public KeyboardEvent(NetworkStream stream, uint playerNum)
         {
             type = stream.ReadByte();
             key = stream.ReadByte();
+
+            if (playerNum == 2)
+            {
+                switch (key)
+                {
+                    case 65: //A
+                        key = 90;
+                        break;
+                    case 66: //B
+                        key = 86;
+                        break;
+                    case 83: //S
+                        key = 88;
+                        break;
+                    case 84: //T
+                        key = 67;
+                        break;
+                    case 38: //UP
+                        key = 75;
+                        break;
+                    case 40: //DOWN
+                        key = 74;
+                        break;
+                    case 37: //LEFT
+                        key = 72;
+                        break;
+                    case 39: //RIGHT
+                        key = 76;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
 
         public void Execute()
@@ -67,17 +101,19 @@ namespace EmulatorRobot
         }
 
         private NetworkStream stream;
-            
-        public Client(NetworkStream stream)
+        private uint playerNum;
+
+        public Client(NetworkStream stream, uint playerNum)
         {
             this.stream = stream;
+            this.playerNum = playerNum;
         }
 
         private void recieveKeyboard()
         {
             while (true)
             {
-                KeyboardEvent key = new KeyboardEvent(stream);
+                KeyboardEvent key = new KeyboardEvent(stream, playerNum);
                 key.Execute();
             }
         }
@@ -136,9 +172,13 @@ namespace EmulatorRobot
     class Server
     {
         private TcpListener tcpListener;
+        private uint numPlayers;
+        private Boolean[] users;
+
         public Server(int port)
         {
             tcpListener = new TcpListener(IPAddress.Any, port);
+            users = new Boolean[2] { false, false };
         }
 
         public void Run()
@@ -149,23 +189,33 @@ namespace EmulatorRobot
 
             while (true)
             {
-                //blocks until a client has connected to the server
-                TcpClient client = tcpListener.AcceptTcpClient();
+                for (int i = 0; i < 2; i++)
+                {
+                    if (!users[i])
+                    {
+                        users[i] = true;
+                        //blocks until a client has connected to the server
+                        TcpClient client = tcpListener.AcceptTcpClient();
 
-                //create a thread to handle communication 
-                //with connected client
-                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleConnection));
-                clientThread.Start(client);
+                        //create a thread to handle communication 
+                        //with connected client
+                        Thread clientThread = new Thread(new ParameterizedThreadStart(HandleConnection));
+                        clientThread.Start(client);
+                        break;
+                    }
+                }
+
             }
         }
 
         private void HandleConnection(object client)
         {
+            numPlayers++;
             Console.WriteLine("Client connected!");
             TcpClient tcpClient = (TcpClient)client;
             NetworkStream clientStream = tcpClient.GetStream();
 
-            Client c = new Client(clientStream);
+            Client c = new Client(clientStream, numPlayers);
             c.Start();
         }
 
